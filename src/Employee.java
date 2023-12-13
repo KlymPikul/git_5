11f53fa1e09deb2f67430f175114c2a4370308cb
KlymPikul <klimpikul1989@gmail.com>
2023-12-13 20:59:42 +0100

    add to Employee

---
+++ src/Employee.java
@@ -0,0 +1,21 @@
+public class Employee extends Person {
+    private double salary;
+
+    public Employee(String firstName, String lastName, double salary) {
+        super(firstName, lastName);
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String info() {
+        return String.format("(%s %s, Salary: %.2f)", super.info(), salary);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
         Person person = new Person("Klym", "Pikul");
         System.out.println(person.info());
-    }
 
     }
-
-
+}
