f662f2b42523f6c8ad0e2f082f027439eb1b2bde
KlymPikul <klimpikul1989@gmail.com>
2023-12-13 20:06:55 +0100

    add Person class

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Employee employee = new Employee("Klyv", "Pikul", 34);
 
-        // Выводим информацию на экран
-        System.out.printf("First Name: %s\n", employee.getFirstName());
-        System.out.printf("Last Name: %s\n", employee.getLastName());
-        System.out.printf("Age: %d\n", employee.getAge());
+        Person person = new Person("Klym", "Pikul");
+        System.out.println(person.info());
     }
+
     }
+
 
